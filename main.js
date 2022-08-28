20519af585f8994d47f50de341261a5d93682872
duralumin02 <sawnaytharpoe14@gmail.com>
2022-08-28 16:43:37 +0630

    finish commit

--- main.js
+++ main.js
@@ -1,4 +1,113 @@
+import 'bootstrap/dist/js/bootstrap.bundle';
 import './style.scss';
+import Typed from 'typed.js';
+import ScrollReveal from 'scrollreveal';
 
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-item');
 
-console.log("what is ur name");
+window.addEventListener('scroll', () => {
+	let current = '';
+	sections.forEach((section) => {
+		let sectionTop = section.offsetTop;
+		let sectionHeight = section.clientHeight;
+
+		if (window.pageYOffset > sectionTop - sectionHeight / 2) {
+			current = section.getAttribute('id');
+		}
+	});
+
+	navLinks.forEach((a) => {
+		a.classList.remove('active');
+		if (a.classList.contains(current)) {
+			a.classList.add('active');
+		}
+	});
+});
+
+var options = {
+	strings: ['Food ....', 'Drink ....'],
+	typeSpeed: 60,
+	backSpeed: 20,
+	backDelay: 1300,
+	loop: true,
+};
+
+new Typed('.element', options);
+
+var slideDown = {
+	distance: '50px',
+	duration: '1000',
+	origin: 'top',
+	interval: '200',
+};
+
+ScrollReveal().reveal('.slide-down', slideDown);
+
+var slideInRight = {
+	distance: '50px',
+	duration: '1000',
+	origin: 'left',
+	interval: '200',
+};
+
+ScrollReveal().reveal('.slide-right', slideInRight);
+
+var sldieInLeft = {
+	distance: '50px',
+	duration: '1000',
+	origin: 'right',
+	interval: '200',
+};
+
+ScrollReveal().reveal('.slide-left', sldieInLeft);
+
+let menuLists = [
+	{
+		id: 1,
+		title: 'Barbecue Salad',
+		description: 'Special Delicious Dish',
+		img: 'public/img/plate1.png',
+		price: 22.2,
+		currency: '$',
+	},
+	{
+		id: 2,
+		title: 'Salad with fish',
+		description: 'Special Delicious Dish',
+		img: 'public/img/plate2.png',
+		price: 30.5,
+		currency: '$',
+	},
+	{
+		id: 3,
+		title: 'Spinach Salad',
+		description: 'Special Delicious Dish',
+		img: 'public/img/plate3.png',
+		price: 42.2,
+		currency: '$',
+	},
+];
+
+let card = document.querySelectorAll('.card');
+
+menuLists.forEach((menu) => {
+	card.forEach((c) => {
+		c.innerHTML = `
+			<img
+				class="img mx-auto mt-4 mb-3"
+				src="${menu.img}"
+				width="160px"
+				alt=""
+			/>
+			<div class="card-body">
+				<p class="card-title fw-semibold fs-5 mb-0">${menu.title}</p>
+				<p class="card-text small">${menu.description}</p>
+				<div class="d-flex justify-content-between align-items-center">
+					<p class="mb-0 fw-bold">${menu.currency} ${menu.price}</p>
+					<button class="btn btn-primary"><i class="bi bi-cart-plus"></i></button>
+				</div>
+			</div>
+		`;
+	});
+});
